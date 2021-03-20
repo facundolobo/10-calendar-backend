7e625bedcf3461a7d17152e8ba5579137625b4e7
Facundo Lobo <facundolobo2@gmail.com>
2021-03-20 15:46:26 -0300

    15. Validaciones del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,52 @@
 
 const {response}= require('express');//para ayudarme con el tipado
+const Usuario = require('../models/Usuario');
 
-const crearUsuario= (req,res= response) =>{ //request , response, agregar eso a resp para el tipado
+const crearUsuario= async(req,res= response) =>{ //request , response, agregar eso a resp para el tipado
 
-    const {name, email, password} = req.body;
+    const {email, password} = req.body;
     
-    //manejo de rrores
-
     
-    res.status(201).json({ //devolvera esto
-        ok: true,
-        msg:'registro',
-        name,
-        email,
-        password
-    })
+        try{
+            // validacion de email unico
+                let usuario = await Usuario.findOne({email});
+                console.log(usuario);
+                if(usuario){
+                    return res.status(400).json({
+                        ok: false,
+                        msg: 'Un usuario existe con ese correo'
+                    });
+                }
+            //--
+
+            //grabar en BBDD
+            usuario=new Usuario(req.body)
+            await usuario.save();
+            //--
+        
+        res.status(201).json({ //devolvera esto
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+           
+        })
+        }catch(error){
+            console.log(error);
+            res.status(500).json({
+                ok: false,
+                msg: 'Por favor hable con el administrador'
+            })
+        }
+
 }
 
 const loginUsuario = (req,res= response) =>{ 
     
-    const {email, password} = req.body; 
+    const {email, password} = req.body;  //obtiene los datos de req
 
     
+
+
     res.status(201).json({ //devolvera esto
         ok: true,
         msg:'login', 
